70c8bed48885cfbec14f67df92e72a6068fb6259
seekerhopeh-afk <seekerhope.h@gmail.com>
2026-09-06 10:14:09 +0700

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import TarotSecretReader from "./TarotSecretReader.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <TarotSecretReader />
-  </React.StrictMode>
-);
